a037aebc307e445ce15ac5ec5955deeed8b8ca37
Dor Sarel <Dor.Sarel@wdc.com>
2018-08-03 19:23:15 +0300

    Removed HelpPage component

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -11,7 +11,6 @@ import Header from '../components/Header';
 import ExpenseDashboard from '../components/ExpenseDashboard';
 import AddExpense from '../components/AddExpense';
 import EditExpense from '../components/EditExpense';
-import HelpExpense from '../components/HelpExpense';
 import NotFound from '../components/NotFound';
 
 const AppRouter = () => (
@@ -22,7 +21,6 @@ const AppRouter = () => (
                 <Route path='/' component={ExpenseDashboard} exact={true} />
                 <Route path='/create' component={AddExpense} />
                 <Route path='/edit/:id' component={EditExpense} />
-                <Route path='/help' component={HelpExpense} />
                 <Route component={NotFound} />
             </Switch>
         </div>
