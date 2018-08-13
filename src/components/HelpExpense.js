de0dfa45e90e28c6218efe633023bc9dff774802
Dor Sarel <Dor.Sarel@wdc.com>
2018-08-13 13:52:59 +0300

    Add public route

--- src/components/HelpExpense.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const HelpExpense = () => (
-    <div>
-        This if from HelpExpensePage component
-    </div>
-);
-
-export default HelpExpense;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -11,6 +11,7 @@ import AddExpense from '../components/AddExpense';
 import EditExpense from '../components/EditExpense';
 import NotFound from '../components/NotFound';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 export const history = createHistory();
 
@@ -18,7 +19,7 @@ const AppRouter = () => (
     <Router history={history}>
         <div>
             <Switch>
-                <Route path='/' component={LoginPage} exact={true} />
+                <PublicRoute path='/' component={LoginPage} exact={true} />
                 <PrivateRoute path='/dashboard' component={ExpenseDashboard} />
                 <PrivateRoute path='/create' component={AddExpense} />
                 <PrivateRoute path='/edit/:id' component={EditExpense} />
